2584b321113a3197086dedff4f156938ad7d75f0
Jonas Stene <jonas@stene.li>
2025-06-02 22:02:17 +0200

    Updated chanel version to mach home manager

--- home.nix
+++ home.nix
@@ -17,14 +17,12 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
+  home.stateVersion = "25.05"; # Please read the comment before changing.
 
-
-  nixpkgs.config.allowUnfreePredicate = pkg:
-    builtins.elem (lib.getName pkg) [
-      # Unfree packages added here
-      "spotify"
-      ];
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "spotify"
+  ];
+         
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
